828a07b5beabaf5a57e75ac27958e3165f0757a5
Tonya <tonyafrankwadel@gmail.com>
2016-07-10 02:07:22 +0000

    saving to github

--- assets/scripts/my-scripts.js
+++ assets/scripts/my-scripts.js
@@ -1,5 +1,5 @@
 // initialize order count (Worksheet Part 1: Step 9a)
-
+var orderCount = 0;
 /* array of drink objects (Worksheet Part 2: Step 1)
    each object has a value key with a camelCase drink name value 
    and a label key with the display text for the drink */
@@ -9,11 +9,15 @@
 
 /* submitOrder function that increments orderCount, gathers and displays order details.
    (Worksheet Part 1: Step 3) */
-  
+var submitOrder = function () {
+    var orderName = "Tonya"//$("#order-form-input").val();
+    console.log(orderName);
+    var drinkName = $("input[type='radio']:checked").val();
   // only add orders if there is room left on the order queue (Worksheet Part 1: Step 9g)
 
   // add order display (Worksheet Part 1: Step 7)
-
+$("#order-details").append("<h1>" + orderName +"would like a"+ drinkName+ "</h1>");
+}
   // call funciton to update drink order count (Worksheet Part 1: Step 9e)
 
 
@@ -24,6 +28,7 @@ $(document).ready(function() {
   // call loadMenu function (Worksheet Part 2: Step 2d)
 
   $('#order-btn').click(function() {
+      submitOrder();
     // call submitOrder function when order button is clicked (Worksheet Part 1: Step 5b)
   });
 });
